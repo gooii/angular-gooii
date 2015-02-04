2085d8e0dedeb556035da31b73411291018f9e48
Martin Wood-Mitrovski <mdk@relivethefuture.com>
2015-02-04 10:06:06 +0100

    bump config

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,8 +1,16 @@
 module.exports = {
-  scripts: {
-    files: ["bower.json"],
-    updateConfigs: ["pkg"],
-    commitFiles: ["-a"],
-    push: false
-  }
+    options: {
+        files             : ['bower.json'],
+        updateConfigs     : [],
+        commit            : true,
+        commitMessage     : 'Release v%VERSION%',
+        commitFiles       : ['bower.json'],
+        createTag         : true,
+        tagName           : '%VERSION%',
+        tagMessage        : 'Version %VERSION%',
+        push              : true,
+        pushTo            : 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace     : false
+    }
 };
